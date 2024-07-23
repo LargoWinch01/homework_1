59a8327cf21909ad8f77f58c5031711c29e36c61
LargoWinch01 <tokorobakidze7@gmail.com>
2024-07-23 17:55:31 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Name is : tornike");
+console.log("Surname is : Robakidze");
+console.log("Age : 100");
+console.warn("Email : Test@gmail.com");
+console.warn("Password : 123456");
+console.log("Phone : 555-555-555");
